8492629ea2cd15ce7cdd06c7a7897ae922926466
David Alberts <mrrockgroin@gmail.com>
2016-01-07 00:17:06 -0700

    Update User Table Seeder

--- database/seeds/Access/UserTableSeeder.php
+++ database/seeds/Access/UserTableSeeder.php
@@ -27,18 +27,9 @@ class UserTableSeeder extends Seeder
         //Add the master administrator, user id of 1
         $users = [
             [
-                'name'              => 'Admin Istrator',
-                'email'             => 'admin@admin.com',
-                'password'          => bcrypt('1234'),
-                'confirmation_code' => md5(uniqid(mt_rand(), true)),
-                'confirmed'         => true,
-                'created_at'        => Carbon::now(),
-                'updated_at'        => Carbon::now(),
-            ],
-            [
-                'name'              => 'Default User',
-                'email'             => 'user@user.com',
-                'password'          => bcrypt('1234'),
+                'name'              => 'David Alberts',
+                'email'             => 'dave@lightsourcecreative.com',
+                'password'          => bcrypt('123456789'),
                 'confirmation_code' => md5(uniqid(mt_rand(), true)),
                 'confirmed'         => true,
                 'created_at'        => Carbon::now(),
