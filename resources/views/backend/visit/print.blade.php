5079ff16ff97cd0c6949ac0f805dd2306a20565d
David Alberts <mrrockgroin@gmail.com>
2016-08-18 13:11:10 -0600

    Change print page title to reflect Session ID and Created at date

--- resources/views/backend/visit/print.blade.php
+++ resources/views/backend/visit/print.blade.php
@@ -1,6 +1,6 @@
 @extends('backend.layouts.print')
 
-@section ('title',  $visit->id)
+@section ('title',  $visit->session_id . '_' . str_replace(' ', '_', $visit->created_at))
 
 @section('content')
         @include('backend.visit.partials.checkinsheet') 
