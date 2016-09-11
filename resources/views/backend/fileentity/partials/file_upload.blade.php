83fdc3696c56a255bba0ff5598244606d1da097e
David Alberts <mrrockgroin@gmail.com>
2016-09-11 04:30:48 -0600

    Update Hopper Client Information

--- resources/views/backend/fileentity/partials/file_upload.blade.php
+++ resources/views/backend/fileentity/partials/file_upload.blade.php
@@ -44,11 +44,22 @@
                 </div><!-- /.info-box -->
 
             </div>
+    </div><!-- /.box-body -->
+</div><!-- /.box -->
+
+<div class="box box-primary">
+    <div class="box-header with-border">
+        <h3 class="box-title">Hopper Desktop Client</h3>
+    </div><!-- /.box-header -->
+    <div class="box-body">		
 		<div v-show="hopperClient" class="alert alert-success">
 			<i class="fa fa-signal" aria-hidden="true"></i> Your Hopper Client is <strong>Online</strong>
 		</div>
 		<div v-else class='alert alert-warning'>
 			<i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Your Hopper Client is <strong>Offline</strong>
 		</div>
+		<p>Hopper Desktop Client is an application that sits on your system, connected to Hopper and linked to your account via your login credentials. Hopper client allows you to use the same File Upload tools as above, but use them faster by taking advantage of native File API tools provided by NodeJS. When you enter the check-in/update page of an Event Session, or enter into the Visit page, Hopper broadcasts this information to your Hopper Client on your desktop, "awakening" your client.</p>
+		<p><strong>You are not required to use Hopper Client -- the same drag and drop area above will accept a file!</strong></p>
     </div><!-- /.box-body -->
 </div><!-- /.box -->
+
